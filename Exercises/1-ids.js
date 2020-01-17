97b7118e0dca574f6d7903466f18770668e9d368
k2miha <k2punk@gmail.com>
2020-01-17 16:28:33 +0300

    make task

--- Exercises/1-ids.js
+++ Exercises/1-ids.js
@@ -1,20 +1,36 @@
 'use strict';
 
 const ids = function* () {
-  const free = ['0'];
+  const obj = { 0: '0' };
+  const free = () => obj;
+  let i = 0;
+  free.shift = () => {
+    let obj1 = {};
+    const firstElem = Object.keys(obj)[0];
+    const deletedItem = obj[firstElem];
+    delete obj[firstElem];
+    obj1 = { obj, deletedItem };
+    return obj1;
+  };
+  free.push = a => {
+    obj[i] = a;
+    i++;
+    return obj;
+  };
   const prepared = { has: false, value: '' };
 
   while (true) {
+    if (i > 30) break;
     if (prepared.has) {
       prepared.has = false;
       yield prepared.value;
     }
     const nextFree = free.shift();
-    free.push('01' + nextFree);
-    free.push('00' + nextFree);
-    prepared.value = '11' + nextFree;
+    free.push('01' + nextFree.deletedItem);
+    free.push('00' + nextFree.deletedItem);
+    prepared.value = '11' + nextFree.deletedItem;
     prepared.has = true;
-    yield '10' + nextFree;
+    yield '10' + nextFree.deletedItem;
   }
 };
 
